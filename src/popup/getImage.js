af4ba8745dd4bba420b20c8badd9091da2390acf
taesik <jts1535@naver.com>
2022-11-01 22:57:05 +0900

    test

--- src/popup/getImage.js
+++ src/popup/getImage.js
@@ -4,10 +4,13 @@ inputElement.addEventListener('change', (e) => {
     imgElement.src = URL.createObjectURL(e.target.files[0]);
 }, false);
 imgElement.onload = function () {
+
     let src = cv.imread(imgElement);
     let dst = new cv.Mat();
-    cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
+    let low = new cv.Mat(src.rows, src.cols, src.type(), [0, 0, 0, 0]);
+    let high = new cv.Mat(src.rows, src.cols, src.type(), [150, 150, 150, 255]);
+    // You can try more different parameters
+    cv.inRange(src, low, high, dst);
     cv.imshow('canvasOutput', dst);
-    src.delete();
-    dst.delete();
+    src.delete(); dst.delete(); low.delete(); high.delete();
 };
