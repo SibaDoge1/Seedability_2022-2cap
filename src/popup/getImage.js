998fd1d186680d1a3e44432ad03e4b7e6ddf3c74
taesik <jts1535@naver.com>
2022-11-03 19:59:25 +0900

    manipulation

--- src/popup/getImage.js
+++ src/popup/getImage.js
@@ -7,13 +7,23 @@ inputElement.addEventListener('change', (e) => {
 
 imgElement.onload = function () {
     let src = cv.imread(imgElement);
+    let src2 = src.clone();
     let dst = new cv.Mat();
+    let dst2 = new cv.Mat();
     // You can try more different parameters
     let low = new cv.Mat(src.rows, src.cols, src.type(), [0, 0, 0, 0]);
     let high = new cv.Mat(src.rows, src.cols, src.type(), [150, 150, 150, 255]);
 
+    let low2 = new cv.Mat(src2.rows, src2.cols, src.type(), [0, 128, 0, 0]);
+    let high2 = new cv.Mat(src2.rows, src2.cols, src.type(), [100, 255, 100, 255]);
+
     cv.inRange(src, low, high, dst);
+    cv.inRange(src2, low2, high2, dst2)
     let mask = dst.clone();
+    let mask2 = dst2.clone();
+    //use inrange function to set mask area
+
+
     for (let i = 0; i < mask.rows; i++) {
         for (let j = 0; j < mask.cols; j++) {
             if (mask.ucharPtr(i, j)[0] === 255) {
@@ -25,7 +35,26 @@ imgElement.onload = function () {
         }
     }
     cv.imshow('canvasOutput', src);
+    for (let i = 0; i < mask2.rows; i++) {
+        for (let j = 0; j < mask2.cols; j++) {
+            if (mask2.ucharPtr(i, j)[0] === 255) {
+                src2.ucharPtr(i, j)[0] = dst2.ucharPtr(i, j)[0]
+                src2.ucharPtr(i, j)[1] = dst2.ucharPtr(i, j)[1]
+                src2.ucharPtr(i, j)[2] = dst2.ucharPtr(i, j)[2]
+                src2.ucharPtr(i, j)[3] = dst2.ucharPtr(i, j)[3]
+            }
+        }
+    }
+    let mani = new cv.Mat();
+
+    cv.add(src, src2, mani);
+
+    cv.imshow('canvasOutput2', mani);
+
     src.delete();
     dst.delete();
+    src2.delete();
+    dst2.delete();
+
 
 };
