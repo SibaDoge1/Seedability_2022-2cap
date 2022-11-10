1f390cef09dfe15f6e369c472b429308f4ece7cb
taesik <jts1535@naver.com>
2022-11-10 18:42:35 +0900

    edge detection

--- src/popup/getImage.js
+++ src/popup/getImage.js
@@ -10,50 +10,82 @@ inputElement2.addEventListener('change', (e) => {
 }, false);
 
 
-imgElement2.onload = function () {
-    let src = cv.imread(imgElement);
-    let src2 = cv.imread(imgElement2);
+function add_pattern(x, y){
     let dst = new cv.Mat();
+    //let bg = new cv.Mat();
     //let dst2 = new cv.Mat();
     // You can try more different parameters
-    let low = new cv.Mat(src.rows, src.cols, src.type(), [0, 128, 0, 0]);
-    let high = new cv.Mat(src.rows, src.cols, src.type(),  [100, 255, 100, 255]);
+    let low = new cv.Mat(x.rows, x.cols, x.type(), [0, 128, 0, 0]);
+    let high = new cv.Mat(x.rows, x.cols, x.type(),  [100, 255, 100, 255]);
 
-    //let low2 = new cv.Mat(src2.rows, src2.cols, src.type(), [0, 128, 0, 0]);
-    //let high2 = new cv.Mat(src2.rows, src2.cols, src.type(), [100, 255, 100, 255]);
+    cv.inRange(x, low, high, dst);
 
-    cv.inRange(src, low, high, dst);
-    //cv.inRange(src2, low2, high2, dst2)
     let mask = dst.clone();
     //use inrange function to set mask area
     let mask_inv = new cv.Mat();
-    
+
     cv.bitwise_not(mask, mask_inv);
 
     cv.imshow('canvasOutput', dst);
-    
-    for (let i = 0; i < mask_inv.rows; i++) {
-        for (let j = 0; j < mask_inv.cols; j++) {
-            if (mask_inv.ucharPtr(i, j)[0] === 255) {
-                src2.ucharPtr(i, j)[0] = dst.ucharPtr(i, j)[0]
-                src2.ucharPtr(i, j)[1] = dst.ucharPtr(i, j)[1]
-                src2.ucharPtr(i, j)[2] = dst.ucharPtr(i, j)[2]
-                src2.ucharPtr(i, j)[3] = dst.ucharPtr(i, j)[3]
+    /*
+    for (let i = 0; i < mask.rows; i++) {
+        for (let j = 0; j < mask.cols; j++) {
+            if (mask.ucharPtr(i, j)[0] === 255) {
+                x.ucharPtr(i, j)[0] = dst.ucharPtr(i, j)[0]
+                x.ucharPtr(i, j)[1] = dst.ucharPtr(i, j)[1]
+                x.ucharPtr(i, j)[2] = dst.ucharPtr(i, j)[2]
+                x.ucharPtr(i, j)[3] = dst.ucharPtr(i, j)[3]
             }
         }
     }
+    */
+    for (let i = 0; i < mask_inv.rows; i++) {
+        for (let j = 0; j < mask_inv.cols; j++) {
+            if (mask_inv.ucharPtr(i, j)[0] === 255) {
+                y.ucharPtr(i, j)[0] = dst.ucharPtr(i, j)[0]
+                y.ucharPtr(i, j)[1] = dst.ucharPtr(i, j)[1]
+                y.ucharPtr(i, j)[2] = dst.ucharPtr(i, j)[2]
+                y.ucharPtr(i, j)[3] = dst.ucharPtr(i, j)[3]
+            }
+        }
+    }
+
     let mani = new cv.Mat();
 
     //cv.add(src, src2, mani);
-    cv.addWeighted(src,0.7,  src2,0.3, 0.0, mani);
+    cv.addWeighted(x,0.7,  y,0.3, 0.0, mani);
+    //cv.bitwise_or(x, y, mani);
 
-    cv.imshow('canvasOutput2', src2);
+    cv.imshow('canvasOutput2', y);
+
     cv.imshow('canvasOutput3', mani);
+
+    x.delete();
+    dst.delete();
+    y.delete();
+
+}
+
+imgElement.onload = function () {
+    let src = cv.imread(imgElement);
+    //let src2 = cv.imread(imgElement2);
+    let edge = new cv.Mat();
+    let dst = new cv.Mat();
+
+    //add_pattern(src, src2);
+    
+    cv.cvtColor(src, src, cv.COLOR_RGB2GRAY, 0);
+	
+	cv.Canny(src, edge, 50, 100, 3, false);
+
+    cv.bitwise_or(src, edge, dst);
+
+
+    cv.imshow('canvasOutput', edge);
 
     src.delete();
     dst.delete();
-    src2.delete();
-    //dst2.delete();
+    
 
 
 };
