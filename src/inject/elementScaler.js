7615bbe5e3e59fc920d714e6278d438a52c37add
SibaDoge1 <rladus753159@gmail.com>
2022-11-11 04:28:33 +0900

    improved Element Zoom 2

--- src/inject/elementScaler.js
+++ src/inject/elementScaler.js
@@ -14,48 +14,79 @@ const originFontSize = 100;
 // };
 
 let targets = []
+let affectedDoms = []
 let parentCache = []
 const blackList = []
+// document.onmouseout = (e)=>{
+//   let element = e.target
 
-document.onmousemove = function (e) {
+  
+//   element.style = element.originStyle;
+//   element.style.transition = "0.5s";
+//   element.isZoomed = false
+// 	targets.forEach(element=>{
+//     element.style = element.originStyle;
+//     element.style.transition = "0.5s";
+//     element.isZoomed = false
+// 	})
+//   targets = []
+//   affectedDoms = []
+// }
+document.onmouseover = function (e) {
+  
 	targets.forEach(element=>{
     element.style = element.originStyle;
     element.style.transition = "0.5s";
     element.isZoomed = false
 	})
   targets = []
+  affectedDoms = []
+  let element = e.target
 
-	let elements = document.elementsFromPoint(e.clientX, e.clientY);
+  if(isVisible(element)){
+    //텍스트일 경우
+    //element.firstChild?.nodeValue != null && element.firstChild?.nodeValue.trim()
+    if(checkText(element)){ 
+      if(isDuplicate(element)) return
+      //console.log(element.firstChild?.nodeValue)
+      let originStyle = element.style
+      if(element.originStyle === undefined){
+        element.originStyle = originStyle;
+        element.originFontSize = window.getComputedStyle(element).fontSize
+      }
+      
 
-  for(var element  of elements){
-		if ((!blackList.includes(element.tagName))){
-      //텍스트일 경우
-      if(element.firstChild?.nodeValue != null && element.firstChild?.nodeValue.replace(/\s/g, "") != ""){ 
-
-        //console.log(element.firstChild?.nodeValue)
-        let originStyle = window.getComputedStyle(element)
-        var fontSize = parseInt(originStyle.fontSize) + 5 + "px";
-        element.style.fontSize = fontSize;
+      var fontSize = changeFontSize(element.originFontSize);
+      element.style.setProperty('font-size', fontSize, 'important');
+      element.style.transition = "0.5s";
+      element.isZoomed = true
+      targets.push(element)
+      
+    }
+    //이미지일 경우(src값이 있을 경우)
+    else if((element.src != null || element.dataset.src != null)){ 
+      if(isDuplicate(element)) return
+        //console.log(element)
+        let originStyle = element.style
+        element.style.transform = "scale(1.3, 1.3)";
         element.originStyle = originStyle;
         element.style.transition = "0.5s";
         element.isZoomed = true
         targets.push(element)
-        
-      }
-      //이미지일 경우(src값이 있을 경우)
-      else if((element.src != null || element.dataset.src != null)){ 
-
-          //console.log(element)
-          let originStyle = window.getComputedStyle(element)
-          element.style.transform = "scale(1.3, 1.3)";
-          element.originStyle = originStyle;
-          element.style.transition = "0.5s";
-          element.isZoomed = true
-          targets.push(element)
-      }
-		}
+    }
   }
 };
+
+function checkText(ele){  
+  if (ele.hasChildNodes()) {
+    for(child of ele.childNodes){
+      if(child.nodeType == 3 && child.nodeValue.trim()){
+        return true
+      }
+    }
+  }
+  return false
+}
 
 const getParents = el => {
   for (var parents = []; el; el = el.parentNode) {
@@ -66,11 +97,64 @@ const getParents = el => {
 };
 
 function isDuplicate(el){
+  if(el.isZoomed === true){
+    return true
+  }
   const parents = getParents(el)
   for(var ele  of parents){
-    if(ele.isZoomed == true){
+    if(ele.isZoomed === true){
       return true
     }
   }
   return false
+}
+
+function isVisible(e) {
+  return !!( e.offsetWidth || e.offsetHeight || e.getClientRects().length );
+}
+
+function isText(e){
+  let child = e.firstChild,
+  texts = [];
+
+  while (child) {
+      if (child.nodeType == 3) {
+          if(child.data.trim()) return true
+      }
+      child = child.nextSibling;
+  }
+  return false
+}
+
+function convertCSSLengthUnit(fromUnitValue, toUnit){
+  let value = fromUnitValue.match(/[0-9]+\.*[0-9]*/g)[0];
+  let unit = fromUnitValue.match(/[a-zA-Z]+/g)[0];
+
+  let frag = document.createRange().createContextualFragment(`
+    <div style='all: initial; pointer-events: none; display: block; position: absolute; border: none; padding: 0; margin: 0; background: rgba(0,0,0,0); color: color: rgba(0,0,0,0); width: 1${unit}; height: 1px;'></div>
+  `);
+  document.body.appendChild(frag);
+  let measureElement = document.body.children[document.body.children.length-1];
+  let toUnitValuePixel = measureElement.getBoundingClientRect().width * value; // X
+  measureElement.remove();
+
+  if(toUnit){
+    let frag = document.createRange().createContextualFragment(`
+      <div style='all: initial; pointer-events: none; display: block; position: absolute; border: none; padding: 0; margin: 0; background: rgba(0,0,0,0); color: color: rgba(0,0,0,0); width: 1${toUnit}; height: 1px;'></div>
+    `);
+    document.body.appendChild(frag);
+    let measureElement = document.body.children[document.body.children.length-1];
+    let valueUnit = measureElement.getBoundingClientRect().width; // Y
+    measureElement.remove();
+
+    // Given: Aem and Bmm with B=1. We know: Aem = Xpx and Bmm = Ypx. Therefore: 1px = Bmm/Y Result: Aem = Xpx = X * 1px = X * Bmm/Y <=> Aem = X * 1mm/Y (with B=1) <=> Aem = X/Ymm
+    return (toUnitValuePixel / valueUnit) + toUnit;
+  }
+  return toUnitValuePixel + 'px';
+}
+
+function changeFontSize(originFontSize){
+  let converted = convertCSSLengthUnit(originFontSize, 'px')
+  var fontSize = parseInt(converted) + 5 + "px";
+  return fontSize
 }
